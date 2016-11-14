900385737fa9c161f851ca285cf157fd07016843
Vaclav Rosecky <xrosecky@gmail.com>
2016-11-14 07:51:51 +0100

    affilation filter simplified

--- modules/mzk/lib/Auth/Process/AffilationFilterLDAP.php
+++ modules/mzk/lib/Auth/Process/AffilationFilterLDAP.php
@@ -28,16 +28,11 @@ class sspmod_mzk_Auth_Process_AffilationFilterLDAP extends SimpleSAML_Auth_Proce
 	public function process(&$request) {
 		$attributes =& $request['Attributes'];
 		if ($attributes["source"][0] == "bupu.mzk.cz" || $attributes["source"][0] == "ldap://ad.mzk.cz:50389") {
-			$attributes["eduPersonScopedAffiliation"][] = "staff@mzk.cz";
 			$attributes["eduPersonAffiliation"][] = "staff";
-			$attributes["eduPersonScopedAffiliation"][] = "member@mzk.cz";
 			$attributes["eduPersonAffiliation"][] = "member";
 			$attributes["uid"] = $attributes["id"];
 		}
 		if (in_array('mzkProxyAccount', $attributes['objectClass'])) {
-			$attributes["eduPersonScopedAffiliation"][] = "member@mzk.cz";
-			$attributes["eduPersonAffiliation"][] = "member";
-			$attributes["eduPersonScopedAffiliation"][] = "member@mzk.cz";
 			$attributes["eduPersonAffiliation"][] = "member";
 		}
 	}
