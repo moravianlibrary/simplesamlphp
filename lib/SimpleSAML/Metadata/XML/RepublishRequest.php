537feead4ff5fb7239b8d838b307f97941ba9f3f
Vaclav Rosecky <xrosecky@gmail.com>
2017-01-31 08:56:51 +0100

    Optional generator of eduidmd:RepublishRequest in SAML2 IdP metadata

---
+++ lib/SimpleSAML/Metadata/XML/RepublishRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+class SimpleSAML_Metadata_XML_RepublishRequest
+{
+
+    const NS = 'http://eduid.cz/schema/metadata/1.0';
+
+    private $targets;
+
+    public function __construct($targets) {
+        $this->targets = $targets;
+    }
+
+    public function toXML(\DOMElement $parent)
+    {
+        $doc = $parent->ownerDocument;
+        $rr = $doc->createElementNS(self::NS, 'eduidmd:RepublishRequest');
+        foreach ($this->targets as $target) {
+            $rt = $doc->createElementNS(self::NS, 'eduidmd:RepublishTarget', $target);
+            $rr->appendChild($rt);
+        }
+        $parent->appendChild($rr);
+    }
+
+}
