309ed58b09f95b12550d22c382de7b745bfc0d8a
Vaclav Rosecky <xrosecky@gmail.com>
2019-04-23 09:06:14 +0200

    indentation in AffilationFilterLDAP.php fixed

--- modules/mzk/lib/Auth/Process/AffilationFilterLDAP.php
+++ modules/mzk/lib/Auth/Process/AffilationFilterLDAP.php
@@ -27,14 +27,14 @@ class sspmod_mzk_Auth_Process_AffilationFilterLDAP extends SimpleSAML_Auth_Proce
 	 */
 	public function process(&$request) {
 		$attributes =& $request['Attributes'];
-	        if ($attributes["source"][0] == "ldap://ad.staff.mzk.cz:389") {
+		if ($attributes["source"][0] == "ldap://ad.staff.mzk.cz:389") {
+			$attributes["uid"] = $attributes["id"];
 			$attributes["eduPersonAffiliation"][] = "staff";
 			$attributes["eduPersonScopedAffiliation"][] = "staff@mzk.cz";
 			$attributes["eduPersonAffiliation"][] = "member";
 			$attributes["eduPersonScopedAffiliation"][] = "member@mzk.cz";
 			$attributes["eduPersonAffiliation"][] = "employee";
 			$attributes["eduPersonScopedAffiliation"][] = "employee@mzk.cz";
-			$attributes["uid"] = $attributes["id"];
 			$attributes["mzkPermission"][] = "wifi";
 		}
 		if (in_array('mzkWifiAccount', $attributes['objectClass'])) {
